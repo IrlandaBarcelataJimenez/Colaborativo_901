c8b6e55e5a97ee7164092bbe96d96cd474396cce
irlanda bj <irlandabarji@gmail.com>
2024-08-19 16:34:31 -0600

    nuevo

---
+++ irlanda.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viajes a México</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Explora con Nosotros</h1>
+        <p>Descubre los destinos más fascinantes de México a precios increíbles.</p>
+        <nav>
+            <ul>
+            <li><a href="index.php">Home</a></li>
+                <li><a href="ofertas.php">Ofertas</a></li>
+                <li><a href="destinos.php">Destinos</a></li>
+                <li><a href="irlanda.php">Irlanda</a></li>
+                <li><a href="vania.php">Vania</a></li>
+                <li><a href="xavi.php">Xavi</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="mejores-playas">
+        <h2>Curriculum</h2>
+        <div class="playa">
+            <img src="imagenes/irlanda.png" alt="Cancún">
+            <h3>Irlanda</h3>
+            <p>Mi nombre completo es: Irlanda Barcelata Jimenez</p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/mexico.png" alt="Playa del Carmen">
+            <h3>Mexicana</h3>
+            <p>Soy Mexicana de nacimiento</p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/año.png" alt="Tulum">
+            <h3>Años</h3>
+            <p>Actualmente tengo 20 años</p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/nanchital.jpg" alt="Puerto Vallarta">
+            <h3>Vivo en Nanchital, Ver.</h3>
+            <p>Mi calle es la calle Xalapa #14.</p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/utsv.png" alt="Playa Colomitos">
+            <h3>UTSV</h3>
+            <p>Actualmente curso la carrera de Tecnologias en la Universidad Tecnologica del Sureste de Veracruz.</p>
+        </div>
+    </section>
+
+    <section id="ofertas">
+        <!-- Aquí podrías agregar las ofertas de viajes -->
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Viajes a México. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
