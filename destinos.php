7c73b215db20d0165c96f7006827e6ff0fc02fd1
IrlandaBarcelataJimenez <98171046+IrlandaBarcelataJimenez@users.noreply.github.com>
2024-08-18 18:21:17 -0600

    Add files via upload

---
+++ destinos.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viajes a México</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Explora otros destinos </h1>
+        <p>Descubre los destinos más fascinantes de México a precios increíbles.</p>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="ofertas.php">Ofertas</a></li>
+                <li><a href="destinos.php">Destinos</a></li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <section id="mejores-playas">
+    <div class="playa">
+            <img src="imagenes/valle_de_bravo.jpg" alt="Valle de Bravo">
+            <h3>Valle de Bravo</h3>
+            <p>Valle de Bravo es conocido por su hermoso lago y sus pintorescas calles coloniales. Es un destino popular para el ecoturismo y actividades al aire libre.</p>
+            <p><strong>Viaje redondo desde $2499 MXN</strong></p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/tequila.jpg" alt="Tequila">
+            <h3>Tequila</h3>
+            <p>Tequila, en Jalisco, es famoso por su producción de tequila. Visita sus campos de agave y disfruta de un tour en una de las muchas destilerías locales.</p>
+            <p><strong>Viaje redondo desde $2499 MXN</strong></p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/real_de_catorce.jpg" alt="Real de Catorce">
+            <h3>Real de Catorce</h3>
+            <p>Real de Catorce es un antiguo pueblo minero en el desierto de San Luis Potosí. Ofrece un ambiente místico y paisajes impresionantes.</p>
+            <p><strong>Viaje redondo desde $2499 MXN</strong></p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/tulum.jpg" alt="Tulum">
+            <h3>Valledolid</h3>
+            <p>Valladolid es una ciudad colonial en Yucatán conocida por su arquitectura histórica y cercanía a las ruinas mayas de Chichén Itzá.</p>
+            <p><strong>Viaje redondo desde $2499 MXN</strong></p>
+        </div>
+        <div class="playa">
+            <img src="imagenes/tequila.jpg" alt="San Cristóbal de las Casas">
+            <h3>San Cristóbal de las Casas</h3>
+            <p>San Cristóbal de las Casas es un encantador pueblo en Chiapas, famoso por su arquitectura colonial y su rica cultura indígena.</p>
+            <p><strong>Viaje redondo desde $2499 MXN</strong></p>
+        </div>
+
+    </section>
+
+    <section id="ofertas">
+        <!-- Aquí podrías agregar las ofertas de viajes -->
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Viajes a México. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
