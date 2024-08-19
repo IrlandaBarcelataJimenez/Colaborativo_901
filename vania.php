487e5126b5a93f016dfc14122e8c5f1af3117c4c
VaniaL <dsm21190111.vlicona@alumnos.utsv.edu.mx>
2024-08-19 16:36:49 -0600

    vania info

---
+++ vania.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viajes a México</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Explora otros destinos </h1>
+        <p>Descubre los destinos más fascinantes de México a precios increíbles.</p>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="ofertas.php">Ofertas</a></li>
+                <li><a href="destinos.php">Destinos</a></li>
+                <li><a href="irlanda.php">Irlanda</a></li>
+                <li><a href="vania.php">Vania</a></li>
+                <li><a href="xavi.php">Xavi</a></li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <section id="mejores-playas">
+    <div class="playa">
+            <img src="imagenes/bambi.jpg" alt="Valle de Bravo">
+            <h3>Nombre: Vania Johana Licona Santiago</h3>
+            <p>Materia: Desarrollo Web Integral</p>
+            <p><strong>Grupo:90</strong></p>
+        </div>
+
+    </section>
+
+    <section id="ofertas">
+        <!-- Aquí podrías agregar las ofertas de viajes -->
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Viajes a México. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
